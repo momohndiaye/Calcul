85acfb3369c0bb0f728af71c4fd5e6977fae1197
momoh Bamba <modoubambandiaye90@gmail.com>
2017-12-11 20:48:39 +0000

    ajout du projet sur le depot github

---
+++ src/main/java/sn/objis/calcul/CalculMetier.java
@@ -0,0 +1,12 @@
+package sn.objis.calcul;
+
+public class CalculMetier {
+	public double somme(double a, double b) {
+		return (a + b);
+	}
+
+	public double produit(double a, double b) {
+		return (a * b);
+	}
+
+}
